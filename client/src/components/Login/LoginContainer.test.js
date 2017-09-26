33e7db56de8cb752ff24fe85a7076d34e95b5fd7
Aaron Llanos <allanos@inmotionsoftware.com>
2017-09-26 14:21:25 -0500

    update tests to pass by passing redux into the components

--- client/src/components/Login/LoginContainer.test.js
+++ client/src/components/Login/LoginContainer.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import Login from './LoginContainer';
+
+import {getStore} from '../../../lib/testSetup';
+const store = getStore()
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Login />, div);
+  ReactDOM.render(<Provider store={store}><Login /></Provider>, div);
 });
